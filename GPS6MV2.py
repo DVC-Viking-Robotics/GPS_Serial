cd7450306d97047a2558d407c79522347a7ba7e4
Brendan <2bndy5@gmail.com>
2019-03-22 19:14:21 -0700

    init commit

---
+++ GPS6MV2.py
@@ -0,0 +1,26 @@
+import serial
+
+# open a "channel" (technically I think its called a "handle") to Serial port
+# on Windows my arduino registers as "COM3"
+# on rasbian the Tx/Rx pins register as '/dev/ttys0'
+ser = serial.Serial('/dev/ttys0')
+while (True):
+    try:
+        # read all data in stream until '\n'
+        x = ser.readline()
+        ''' 
+        # windows com port uses CR+LF, so del first char
+        # transform the saved bytearray into a list
+        x = list(x)
+        # delete first garbage character
+        del x[0]
+        # transform back into bytearray, then decode into string format
+        x = bytes(x).decode('utf-8')
+        '''
+        # on raspbian first char is not garbage
+        x = x.decode('utf-8')
+        #ready to print or do whatever you want (like parsing a string)
+        print(x)
+    except KeyboardInterrupt:
+        break
+
