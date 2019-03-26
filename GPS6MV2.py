b68d3ddaa7e5c1b7acf79c4cf74c39911361683b
Brendan <2bndy5@gmail.com>
2019-03-26 16:21:58 -0700

    turned into a self-exec able class

--- GPS6MV2.py
+++ GPS6MV2.py
@@ -3,24 +3,40 @@ import serial
 # open a "channel" (technically I think its called a "handle") to Serial port
 # on Windows my arduino registers as "COM3"
 # on rasbian the Tx/Rx pins register as '/dev/ttyS0'
-ser = serial.Serial('/dev/ttyS0')
-while (True):
-    try:
-        # read all data in stream until '\n'
-        x = ser.readline()
-        # transform the saved bytearray into a list
-        x = list(x)
-        # delete first garbage character
-        del x[0]
-        # transform back into bytearray, then decode into string format
-        x = bytes(x).decode('utf-8')
-        #ready to print or do whatever you want (like parsing a string)
-        if (x.find('GPGLL') != -1):
-            N_start = x.find(',') + 1
-            N_end = x.find(',', N_start)
-            W_start = x.find('N,') + 3
-            W_end = x.find(',', W_start)
-            print('N', x[N_start:N_end], '; W', x[W_start:W_end])
-    except KeyboardInterrupt:
-        break
+
+class GPS():
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyS0')
+        self.north = 0.0
+        self.west = 0.0
+        self.line = ""
+
+    def getCoords(self):
+        found = False
+        while(not found):
+            self.line = self.ser.readline()
+            self.line = list(self.line)
+            del self.line[0]
+            self.line = bytes(self.line).decode('utf-8')
+            if sefl.line.find('GPGLL') != 1:
+                found = True
+        N_start = x.find(',') + 1
+        N_end = x.find(',', N_start)
+        W_start = x.find('N,') + 3
+        W_end = x.find(',', W_start)
+        self.north = self.line[N_start:N_end]
+        self.west = self.line[W_start:W_end]
+
+    def __del__(self):
+        del self.ser, self.north, self.west, self.line 
+
+if __name__ == "__main__":
+    gps = GPS()
+    while (True):
+        try:
+            gps.getCoords()
+            print('N', gps.north, '; W', gps.west)
+        except KeyboardInterrupt:
+            del gps
+            break
 
