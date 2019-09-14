325134ab79dd520aec8a6113859c57154f72dd0e
Brendan <2bndy5@gmail.com>
2019-09-14 15:06:36 -0700

    docs build fine, but need a lot of polish

--- README.rst
+++ README.rst
@@ -14,26 +14,30 @@ Installation
 Currenty, there is no plan to deploy this single module library to pypi.
 Instead, make sure the `py-serial library <https://pypi.org/project/pyserial/>`_ is install via:
 
-.. code:: shell
+.. code-block:: shell
+
     pip3 install pyserial
 
 Some cases may require the command be prefixed with ``sudo `` or appended with `` --user``.
 
 Additionally, if you're going to use the GPIO pins, ``RX`` and ``TX``, you must ensure that the ``serial`` interface is enabled by running:
 
-.. code:: shell
+.. code-block:: shell
+
     sudo raspi-config
 
 .. important:: make sure that the ``serial console`` feature is disabled. Otherwise, any data sent or received over these GPIO pins will be forwarded to a TTY console session if ``serial console`` feature is enabled (meaning this library will not be able to access the GPS module data).
 
 It is worth noting that the port address for the GPIO serial pins is ``/dev/ttyS0``. If you are using a USB connection, the address can be looked up using the py-serial's tools module:
 
-.. code:: shell
+.. code-block:: shell
+
     python3 -m serial.tools.list_ports
 
 You can then test which port in the outputted list is the GPS module by entering:
 
-.. code:: shell
+.. code-block:: shell
+
     python3 -m serial.tools.miniterm /dev/ttyS0
 
 where you replace the ``/dev/ttyS0`` part with the address you're testing. To exit the miniterm application use ``ctrl+[``
--- gps_serial.py
+++ gps_serial.py
@@ -4,9 +4,9 @@ GPS_SERIAL
 
 Yet another NMEA sentence parser for serial UART based GPS modules. This implements the threaded module for [psuedo] asynchronous applications. CAUTION: The individual satelite info is being ignored until we decide to support capturing it from the GPS module's output.
 """
-import serial
 import time
 import threading
+import serial
 
 # default location hard-coded to DVC Engineering buildings' courtyard
 DEFAULT_LOC = {'lat': 37.96713657090229, 'lng': -122.0712176165581}
@@ -101,13 +101,12 @@ class GPS_SERIAL():
     @property
     def hdop(self):
         return self._hdop
-    
 
-    def _parse_line(self, str):
+    def _parse_line(self, string):
         found = False
-        if str.find('GLL') != -1:
+        if string.find('GLL') != -1:
             found = True
-            arr = str.rsplit(',')[1:]
+            arr = string.rsplit(',')[1:]
             # print(repr(arr))
             self._lat = convert2deg(arr[0])
             if arr[1] != 'N' and arr[1] is not None:
@@ -115,10 +114,10 @@ class GPS_SERIAL():
             self._lng = convert2deg(arr[2])
             if arr[3] != 'E' and arr[3] is not None:
                 self._lng *= -1.0
-            typeState = {'A': 'data valid', 'V': 'Data not valid'}
-            self._data_status = typeState[arr[5]]
-        elif str.find('VTG') != -1:
-            arr = str.rsplit(',')[1:]
+            type_state = {'A': 'data valid', 'V': 'Data not valid'}
+            self._data_status = type_state[arr[5]]
+        elif string.find('VTG') != -1:
+            arr = string.rsplit(',')[1:]
             if len(arr[0]) > 1:
                 self._course["true"] = float(arr[0])
             if len(arr[1]) > 1:
@@ -127,29 +126,29 @@ class GPS_SERIAL():
                 self._speed["knots"] = float(arr[2])
             if len(arr[3]) > 1:
                 self._speed["kmph"] = float(arr[3])
-        elif str.find('GGA') != -1:
-            typeState = [
+        elif string.find('GGA') != -1:
+            type_state = [
                 "Fix Unavailable", "Valid Fix (SPS)", "Valid Fix (GPS)", "unknown1", "unknown2", "unknown3"]
-            arr = str.rsplit(',')[1:]
-            self._sat["quality"] = typeState[int(arr[5])]
+            arr = string.rsplit(',')[1:]
+            self._sat["quality"] = type_state[int(arr[5])]
             self._sat["view"] = int(arr[6])
             if len(arr[8]) > 1:
                 self._altitude = float(arr[8])
-        elif str.find('GSA') != -1:
-            arr = str.rsplit(',')[1:]
-            typeFix = ["No Fix", "2D", "3D"]
-            self._fix = typeFix[int(arr[1]) - 1]
+        elif string.find('GSA') != -1:
+            arr = string.rsplit(',')[1:]
+            type_fix = ["No Fix", "2D", "3D"]
+            self._fix = type_fix[int(arr[1]) - 1]
             self._pdop = float(arr[14])
             self._hdop = float(arr[15])
             self._vdop = float(arr[16][:-3])
-        elif str.find('RMC') != -1:
+        elif string.find('RMC') != -1:
             status = {"V": "Warning", "A": "Valid"}
-            arr = str.rsplit(',')[1:]
+            arr = string.rsplit(',')[1:]
             self._rx_status = status[arr[1]]
             if len(arr[0]) > 1 and len(arr[8]) > 1:
                 self._utc = time.struct_time((2000+int(arr[8][4:6]), int(arr[8][2:4]), int(arr[8][0:2]), int(arr[0][0:2]), int(arr[0][2:4]), int(arr[0][4:6]), 0, 0, -1))
-        elif str.find('GSV') != -1:
-            arr = str.rsplit(',')[1:]
+        elif string.find('GSV') != -1:
+            arr = string.rsplit(',')[1:]
             self._sat['connected'] = arr[0]
             # ignoring data specific to individual satelites
             # self.elevation = int(arr[4])
@@ -157,7 +156,7 @@ class GPS_SERIAL():
             # print('sat["view"]:', self.sat["connected"], 'elevation:', self.elevation, 'Azimuth:', self.azimuth)
         return found
 
-    def _threaded_Read(self, raw):
+    def _threaded_read(self, raw):
         found = False
         while not found:
             self._line = self._ser.readline()
@@ -170,7 +169,7 @@ class GPS_SERIAL():
             # found = true if gps coordinates are captured
             found = self._parse_line(self._line)
 
-    def getData(self, raw=False):
+    def get_data(self, raw=False):
         """
         This function only starts the process of parsing the data from a GPS module (if any).
 
@@ -179,53 +178,9 @@ class GPS_SERIAL():
         :returns: the last latitude and longitude coordinates obtained from either object instantiation (zero values) or previously completed parsing of GPS data.
         """
         if not self._dummy:
-            if self._gps_thread != None:
+            if self._gps_thread is not None:
                 self._gps_thread.join()
             self._gps_thread = threading.Thread(
-                target=self._threaded_Read, args=[raw])
+                target=self._threaded_read, args=[raw])
             self._gps_thread.start()
         return {"lat": self.lat, "lng": self.lng}
-
-
-if __name__ == "__main__":
-    # handle cmd line args
-    import os
-    import argparse
-    # add description to program's help screen
-    parser = argparse.ArgumentParser(
-        description='testing purposes. Please try using quotes to encompass values. ie "COM5" or "/dev/ttyS0"')
-    gps_defaults = 'com6'
-    parser.add_argument('--p', default=gps_defaults,
-                        help='Select serial port address. ie "COM3" or "/dev/ttyS0"')
-
-    class args():
-        def __init__(self):
-            parser.parse_args(namespace=self)
-    cmd = args()
-    # finish get cmd line args
-
-    gps = GPS_SERIAL(cmd.p)
-    # gps = GPS_SERIAL('/dev/ttyS0')
-    while True:
-        try:
-            gps.getData() # pass `1` or `true` to print raw data from module
-            if gps.rx_status.startswith('Valid'):
-                print('RxStatus:', gps.rx_status, 'FixType:', gps.fix)
-                print('satelites\' quality:', gps.sat["quality"])
-                print('satelites connected:', gps.sat["connected"])
-                print('satelites in view:', gps.sat['view'])
-                print('Course True North:', gps.course["true"], 'degrees')
-                print('Course Magnetic North:', gps.course["mag"], 'degrees')
-                print('speed in knots:', gps.speed["knots"], 'speed in kmph:', gps.speed["kmph"])
-                print('Altitude:', gps.altitude, 'meters')
-                print('UTC: {}/{}/{} {}:{}:{}'.format(gps.utc[1], gps.utc[2], gps.utc[0], gps.utc[3], gps.utc[4], gps.utc[5]))
-                print('lat:', gps.lat, 'lng:', gps.lng)
-                print('position dilution of precision:', gps.pdop, 'meters')
-                print('horizontal dilution of precision:', gps.hdop, 'meters')
-                print('vertical dilution of precision:', gps.vdop, 'meters\n')
-            else:
-                print('Waiting for gps fix')
-            time.sleep(1)
-        except KeyboardInterrupt:
-            del gps
-            break
